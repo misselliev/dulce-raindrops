2ff09529b896a513829f401ab22116d8c86e14a1
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-04 17:48:46 -0700

    refactoring getting dom elements

--- src/js/setup.js
+++ src/js/setup.js
@@ -4,6 +4,28 @@ const setup = {
   setFavicon: () => {
     const setFav = document.getElementById('favicon');
     setFav.href = fav;
+  },
+  setElements: () => {
+    let info = document.getElementById('info');
+    let location = document.getElementById('location');
+    let event = document.getElementById('event');
+    let icon = document.getElementById('icon');
+    let tempBox = document.getElementById('temperature');
+    let temp = document.getElementById('temp-num');
+    let degree = document.getElementById('degree');
+    let description = document.getElementById('description');
+    const input = document.querySelector('input');
+    return {
+      info,
+      location,
+      event,
+      icon,
+      tempBox,
+      temp,
+      degree,
+      description,
+      input
+    };
   }
 };
 export default setup;
